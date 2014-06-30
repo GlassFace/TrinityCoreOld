8f46a9ff361de3003e2909e9d3b1806a3fd4fb84
lasyan3 <lasyan3@hotmail.com>
2014-06-30 17:08:48 +0200

    Added loot conditions for player:
     - Loot only recipes the player doesn't already know
     - Loot only recipes for player's profession
     - Loot only equipment wearable by the player

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1106,8 +1106,12 @@ void World::LoadConfigSettings(bool reload)
     m_visibility_notify_periodInInstances = sConfigMgr->GetIntDefault("Visibility.Notify.Period.InInstances",   DEFAULT_VISIBILITY_NOTIFY_PERIOD);
     m_visibility_notify_periodInBGArenas = sConfigMgr->GetIntDefault("Visibility.Notify.Period.InBGArenas",    DEFAULT_VISIBILITY_NOTIFY_PERIOD);
 
-	m_float_configs[CONFIG_LOOT_ONLY_FOR_PLAYER] = sConfigMgr->GetBoolDefault("Custom.LootOnlyForPlayer", false);
-	
+	m_bool_configs[CONFIG_LOOT_ONLY_FOR_PLAYER] = sConfigMgr->GetBoolDefault("Custom.LootOnlyForPlayer", false);
+	m_float_configs[CONFIG_MINRATE_DROP_ITEM_RARE] = sConfigMgr->GetFloatDefault("Custom.MinimumRate.Drop.Item.Rare", 1);
+	m_float_configs[CONFIG_MINRATE_DROP_ITEM_EPIC] = sConfigMgr->GetFloatDefault("Custom.MinimumRate.Drop.Item.Rare", 1);
+	m_float_configs[CONFIG_MINRATE_DROP_ITEM_LEGEND] = sConfigMgr->GetFloatDefault("Custom.MinimumRate.Drop.Item.Rare", 1);
+	m_float_configs[CONFIG_MINRATE_DROP_ITEM_ART] = sConfigMgr->GetFloatDefault("Custom.MinimumRate.Drop.Item.Rare", 1);
+
 	///- Load the CharDelete related config options
     m_int_configs[CONFIG_CHARDELETE_METHOD] = sConfigMgr->GetIntDefault("CharDelete.Method", 0);
     m_int_configs[CONFIG_CHARDELETE_MIN_LEVEL] = sConfigMgr->GetIntDefault("CharDelete.MinLevel", 0);
