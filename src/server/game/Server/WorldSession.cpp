29f88818ab1bdb0a7c6c95e4ef4a1f26671f2d1d
lasyan3 <lasyan3@hotmail.com>
2014-07-08 22:49:43 +0200

    NPCBots 4

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -447,6 +447,10 @@ void WorldSession::LogoutPlayer(bool save)
     m_playerLogout = true;
     m_playerSave = save;
 
+    //npcbot - free all bots and remove from botmap
+    _player->RemoveAllBots();
+    //end npcbots
+
     if (_player)
     {
         if (uint64 lguid = _player->GetLootGUID())
